9d1fdfa25198b7d86f8783ed7f85b4096144da39
ChikoShidori <degnan200@gmail.com>
2021-06-29 16:49:33 +0100

    Added properties

--- src/structure/commands/Command.ts
+++ src/structure/commands/Command.ts
@@ -1,17 +1,43 @@
 import { WAChatUpdate } from '@adiwajshing/baileys';
 import { SharkError } from '../../util';
-import SharkModule, { ModuleOptions } from '../SharkModule';
+import SharkModule from '../SharkModule';
+
+declare interface CommandOptions {
+  alias: string[];
+  ownerOnly?: boolean;
+  category?: string;
+  cooldown?: number;
+  ratelimit?: number;
+  prefix?: string | string[];
+  lock?: boolean;
+  ignoreCooldown?: string | string[];
+}
 
 export class Command extends SharkModule {
-  aliases: string | string[];
-  prefix: string;
-  constructor(id: string, options: ModuleOptions) {
-    super(id, options);
-    this.aliases = 'ss';
+  aliases: string[] | [];
+  prefix: string | string[];
+  ownerOnly: boolean;
+  cooldown: number;
+  ratelimit: number;
+  lock: boolean;
+  constructor(id: string, options: CommandOptions) {
+    super(id, { category: options.category });
+
+    this.aliases = options.alias || [];
+
+    this.ownerOnly = Boolean(options.ownerOnly || false);
+
+    this.cooldown = options.cooldown;
+
+    this.ratelimit = options.ratelimit;
+
+    this.prefix = options.prefix;
+
+    this.lock = options.lock;
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  public async exec(message: WAChatUpdate, args?: any) {
+  public async exec(message: WAChatUpdate, args?: any): Promise<any> {
     throw new SharkError('NOT_IMPLEMENTED', this.constructor.name, 'exec');
   }
 }

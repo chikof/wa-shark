1271378441cbf956428043195be777b516892316
ChikoShidori <degnan200@gmail.com>
2021-07-01 21:43:55 +0100

    Listener Handler added

---
+++ src/structure/listeners/Listener.ts
@@ -0,0 +1,25 @@
+import { EventEmitter } from 'events';
+
+import { SharkModule } from '../SharkModule';
+import { SharkError } from '../../util';
+import { ListenerOptions } from '../../util/types';
+
+export class Listener extends SharkModule {
+  public emitter: string | EventEmitter;
+  public event: string;
+  public type: string;
+
+  constructor(id: string, options?: ListenerOptions) {
+    super(id, { category: options.category });
+
+    this.emitter = options.emitter;
+
+    this.event = options.event || id;
+
+    this.type = options.type || 'on';
+  }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public async exec(...args: any[]): Promise<any> {
+    throw new SharkError('NOT_IMPLEMENTED', this.constructor.name, 'exec');
+  }
+}
---
+++ src/structure/listeners/ListenerHandler.ts
@@ -0,0 +1,93 @@
+import { WAConnection } from '@adiwajshing/baileys';
+
+import { EventEmitter } from 'events';
+
+import { Collection, SharkError, Util } from '../../util';
+import { SharkHandlerOptions } from '../../util/types';
+import { SharkHandler } from '../SharkHandler';
+import { Listener } from './Listener';
+
+const { isEventEmitter } = Util;
+
+export class ListenerHandler extends SharkHandler {
+  public emitters: Collection<string, EventEmitter>;
+  public declare modules: Collection<string, Listener>;
+
+  constructor(client: WAConnection, options?: SharkHandlerOptions) {
+    super(client, {
+      directory: options.directory,
+      classToHandle: options.classToHandle || Listener,
+      extensions: options.extensions || ['.js', '.ts'],
+      automateCategories: options.automateCategories,
+      loadFilter: options.loadFilter,
+    });
+
+    this.emitters = new Collection();
+
+    this.modules = new Collection();
+
+    this.emitters.set('client', this.client);
+  }
+
+  public register(listener: Listener, filepath: string) {
+    super.register(listener, filepath);
+    listener.exec = listener.exec.bind(listener);
+    this.addToEmitter(listener.id);
+    return listener;
+  }
+
+  public deregister(listener: Listener) {
+    this.removeFromEmitter(listener.id);
+    super.deregister(listener);
+  }
+
+  public addToEmitter(id: string) {
+    const listener = this.modules.get(id.toString());
+    if (!listener) {
+      throw new SharkError('MODULE_NOT_FOUND', this.classToHandle.constructor.name, id);
+    }
+
+    const emitter = isEventEmitter(listener.emitter)
+      ? (listener.emitter as EventEmitter)
+      : this.emitters.get(listener.emitter as string);
+
+    if (!isEventEmitter(emitter)) {
+      throw new SharkError('INVALID_TYPE', 'emitter', 'EventEmitter', true);
+    }
+
+    if (listener.type === 'once') {
+      emitter.once(listener.event, listener.exec);
+      return listener;
+    }
+
+    emitter.on(listener.event, listener.exec);
+    return listener;
+  }
+
+  public removeFromEmitter(id: string) {
+    const listener = this.modules.get(id.toString());
+    if (!listener) {
+      throw new SharkError('MODULE_NOT_FOUND', this.classToHandle.constructor.name, id);
+    }
+
+    const emitter = isEventEmitter(listener.emitter)
+      ? (listener.emitter as EventEmitter)
+      : this.emitters.get(listener.emitter as string);
+
+    if (!isEventEmitter(emitter)) {
+      throw new SharkError('INVALID_TYPE', 'emitter', 'EventEmitter', true);
+    }
+
+    emitter.removeListener(listener.event, listener.exec);
+    return listener;
+  }
+
+  public setEmitters(emitters: { [x: string]: EventEmitter }) {
+    for (const [key, value] of Object.entries(emitters)) {
+      if (!isEventEmitter(value)) throw new SharkError('INVALID_TYPE', key, 'EventEmitter', true);
+      this.emitters.set(key, value);
+    }
+
+    return this;
+  }
+}

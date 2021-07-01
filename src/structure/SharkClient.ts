a73c149dab5aab7289d9cd40e5255ad19061bae2
ChikoShidori <degnan200@gmail.com>
2021-07-01 21:46:15 +0100

    Name correction

---
+++ src/structure/SharkClient.ts
@@ -0,0 +1,39 @@
+import { WAConnection, WAConnectOptions } from '@adiwajshing/baileys';
+
+import { existsSync, writeFileSync } from 'fs';
+
+export interface SharkOptions {
+  ownerID: string | string[];
+  session: string;
+}
+
+export class SharkClient extends WAConnection {
+  public ownerID: string | string[];
+  session: string;
+
+  constructor(options: SharkOptions, clientOptions?: WAConnectOptions) {
+    super();
+
+    this.ownerID = options.ownerID;
+    if (clientOptions) this.connectOptions = clientOptions;
+
+    this.session = options.session;
+
+    if (existsSync(this.session)) {
+      this.loadAuthInfo(this.session);
+    } else {
+      this.on('open', () => {
+        writeFileSync(this.session, JSON.stringify(this.base64EncodedAuthInfo()), {
+          encoding: 'utf8',
+        });
+      });
+    }
+  }
+
+  public isOwner(user: string): boolean {
+    const id = this.contactAddOrGet(user);
+    return Array.isArray(this.ownerID) ? this.ownerID.includes(id.jid) : id.jid === this.ownerID;
+  }
+}
+
+export default SharkClient;

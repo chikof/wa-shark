b5eed4e4d8e886fb22af19926d40ad70fa22c987
ChikoShidori <degnan200@gmail.com>
2021-07-01 21:49:59 +0100

    Command Handler now working

--- src/structure/commands/CommandHandler.ts
+++ src/structure/commands/CommandHandler.ts
@@ -1,37 +1,43 @@
 import { WAChatUpdate, WAConnection } from '@adiwajshing/baileys';
 
 import { Collection, SharkError, Util } from '../../util';
+import { SharkHandlerOptions } from '../../util/types';
 import { SharkHandler } from '../SharkHandler';
 import { Command } from './Command';
 
 declare interface SharkOptions {
-  directory: string;
-  classToHandle?: Command;
-  automateCategories?: any;
-  loadFilter?: () => boolean;
+  loadfilter?: () => boolean;
   blockClient?: boolean;
   storeMessages?: boolean;
   defaultCooldown?: number;
-  ignoreCooldown?: any;
-  prefix?: any;
-  aliasReplacement: string | RegExp;
+  ignoreCooldown?: () => string | string[];
+  prefix?: () => string | string[];
+  aliasReplacement?: string | RegExp;
 }
 
-export class CommandHandler extends SharkHandler {
-  aliases: Collection<string, string>;
-  aliasReplacement: string | RegExp;
-  prefixes: Collection<any, any>;
-  blockClient: boolean;
-  storeMessages: boolean;
-  ignoreCooldown: any;
-  cooldown: Collection<unknown, unknown>;
-  defaultCooldown: number;
-  prefix: any;
-  inhibitorHandler: any;
-  cooldowns: Collection<any, any>;
-  constructor(client: WAConnection, options?: SharkOptions) {
-    super(client, { directory: options.directory });
+const { intoArray, intoCallable, prefixCompare } = Util;
 
+export class CommandHandler extends SharkHandler {
+  public aliases: Collection<string, string>;
+  public aliasReplacement: string | RegExp;
+  public prefixes: Collection<any, any>;
+  public blockClient: boolean;
+  public storeMessages: boolean;
+  public ignoreCooldown: () => string | string[];
+  public cooldown: Collection<unknown, unknown>;
+  public defaultCooldown: number;
+  public prefix: () => string | string[];
+  public inhibitorHandler: any;
+  public cooldowns: Collection<any, any>;
+
+  constructor(client: WAConnection, options?: SharkOptions & SharkHandlerOptions) {
+    super(client, {
+      directory: options.directory,
+      extensions: options.extensions || ['.js', '.ts'],
+    });
+
+    options.classToHandle = options.classToHandle ? options.classToHandle : Command;
+    this.classToHandle = options.classToHandle;
     if (
       options.classToHandle.constructor.prototype instanceof Command ||
       this.classToHandle.constructor.prototype === Command
@@ -72,14 +78,22 @@ export class CommandHandler extends SharkHandler {
   }
 
   public setup() {
-    this.client.on('open', (w) => {
-      w;
+    this.client.on('open', () => {
+      this.client.on('chat-update', async (m) => {
+        this.handle(m);
+      });
     });
+  }
+
+  public async handle(message: WAChatUpdate) {
+    const parsed = await this.parseCommand(message);
+    if (parsed.command) {
+      this.runCommand(message, parsed.command, parsed.content);
+    }
   }
 
   public register(command: Command, filepath: string) {
     super.register(command, filepath);
-
     for (let alias of command.aliases) {
       const conflict = this.aliases.get(alias.toLowerCase());
       if (conflict) throw new SharkError('ALIAS_CONFLICT', alias, command.id, conflict);
@@ -91,8 +105,10 @@ export class CommandHandler extends SharkHandler {
 
         if (replacement !== alias) {
           const replacementConflict = this.aliases.get(replacement);
-          if (replacementConflict)
+          if (replacementConflict) {
             throw new SharkError('ALIAS_CONFLICT', replacement, command.id, replacementConflict);
+          }
+
           this.aliases.set(replacement, command.id);
         }
       }
@@ -122,9 +138,7 @@ export class CommandHandler extends SharkHandler {
       }
 
       if (newEntry) {
-        this.prefixes = this.prefixes.sort((aVal, bVal, aKey, bKey) =>
-          Util.prefixCompare(aKey, bKey),
-        );
+        this.prefixes = this.prefixes.sort((aVal, bVal, aKey, bKey) => prefixCompare(aKey, bKey));
       }
     }
   }
@@ -136,7 +150,9 @@ export class CommandHandler extends SharkHandler {
 
       if (this.aliasReplacement) {
         const replacement = alias.replace(this.aliasReplacement, '');
-        if (replacement !== alias) this.aliases.delete(replacement);
+        if (replacement !== alias) {
+          this.aliases.delete(replacement);
+        }
       }
     }
 
@@ -163,7 +179,7 @@ export class CommandHandler extends SharkHandler {
     super.deregister(command);
   }
 
-  public runCooldowns(message: WAChatUpdate, command) {
+  public runCooldowns(message: WAChatUpdate, command: Command) {
     const ignorer = command.ignoreCooldown || this.ignoreCooldown;
     const isIgnored = Array.isArray(ignorer)
       ? ignorer.includes(message.jid)
@@ -187,6 +203,7 @@ export class CommandHandler extends SharkHandler {
           if (this.cooldowns.get(id)[command.id]) {
             clearTimeout(this.cooldowns.get(id)[command.id].timer);
           }
+
           this.cooldowns.get(id)[command.id] = null;
 
           if (!Object.keys(this.cooldowns.get(id)).length) {
@@ -216,5 +233,75 @@ export class CommandHandler extends SharkHandler {
     this.emit('commandStarted', message, command, args);
     const ret = await command.exec(message, args);
     this.emit('commandFinished', message, command, args, ret);
+  }
+
+  public async parseCommand(message: WAChatUpdate) {
+    let prefixes = intoArray(await intoCallable(this.prefix)(message));
+    const allowMention = await intoCallable(this.prefix)(message);
+
+    if (allowMention) {
+      const mentions = [`@${this.client.user.jid}`];
+      prefixes = [...mentions, ...prefixes];
+    }
+
+    prefixes.sort(prefixCompare);
+    return this.parseMultiplePrefixes(
+      message,
+      prefixes.map((p) => [p, null]),
+    );
+  }
+
+  public parseMultiplePrefixes(message: WAChatUpdate, pairs: any[]) {
+    const parses = pairs.map(([prefix, cmds]) => this.parseWithPrefix(message, prefix, cmds));
+    const result = parses.find((parsed) => parsed && parsed.command);
+
+    if (result) {
+      return result;
+    }
+
+    const guess = parses.find((parsed) => parsed && parsed.prefix != null);
+    if (guess) {
+      return guess;
+    }
+
+    return {};
+  }
+
+  public parseWithPrefix(
+    message: WAChatUpdate,
+    prefix: string,
+    associatedCommands: Set<string> = null,
+  ) {
+    if (!message.messages) return;
+    const messageContent = message.messages.first.message.conversation;
+    const lowerContent = messageContent.toLowerCase();
+    if (!lowerContent.startsWith(prefix.toLowerCase())) {
+      return {};
+    }
+
+    const endOfPrefix = lowerContent.indexOf(prefix.toLowerCase()) + prefix.length;
+    const startOfArgs = messageContent.slice(endOfPrefix).search(/\S/) + prefix.length;
+    const alias = messageContent.slice(startOfArgs).split(/\s{1,}|\n{1,}/)[0];
+    const command = this.findCommand(alias);
+    const content = messageContent.slice(startOfArgs + alias.length + 1).trim();
+    const afterPrefix = messageContent.slice(prefix.length).trim();
+
+    if (!command) {
+      return { prefix, alias, content, afterPrefix };
+    }
+
+    if (associatedCommands == null) {
+      if (command.prefix != null) {
+        return { prefix, alias, content, afterPrefix };
+      }
+    } else if (!associatedCommands.has(command.id)) {
+      return { prefix, alias, content, afterPrefix };
+    }
+
+    return { command, prefix, alias, content, afterPrefix };
+  }
+
+  public findCommand(name: string) {
+    return this.modules.get(this.aliases.get(name.toLowerCase()));
   }
 }
